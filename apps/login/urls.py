23a996ce3a62fa01b42785bd34b2e32c1adcc9c8
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2023-03-05 19:14:03 +0200

    log in

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import signup_view
+from .views import signup_view, login_view
 
 app_name = 'login'
 
 urlpatterns = [
-    # path('', ),
+    path('', login_view, name='login'),
     path('signup', signup_view, name='signup'),
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -15,3 +15,24 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'sign_up.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                # return redirect('')
+                return HttpResponse('Logged in!')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        # return redirect('')
+        return HttpResponse('Logged Out!')
